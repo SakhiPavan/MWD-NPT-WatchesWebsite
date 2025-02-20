57a1b0fb1f1deb0a6b37ed7b3b8acdbbf3d79c9e
Aniket Kshirsagar <kshirsagarask19@gmail.com>
2025-02-19 21:51:45 -0500

    product_details_commit

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { toast } from 'sonner';
 
 
 const selectedProduct = {
@@ -24,12 +25,39 @@ const selectedProduct = {
 
 const ProductDetails = () => {
     const [mainImage, setMainImage] = useState("");
+    const [selectedSize, setSelectedSize] = useState("");
+    const [selectedColor, setSelectedColor] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [isButtonDisabled, setIsButtonDisabled ] = useState(false);
 
     useEffect(() => {
       if(selectedProduct?.images?.length > 0) {
         setMainImage(selectedProduct.images[0].url)
       }
     }, [selectedProduct]);
+
+    const handleQuantityChange = (action) => {
+      if(action === "plus") setQuantity((prev) => prev + 1);
+      if (action ==="minus" && quantity > 1) setQuantity((prev) => prev - 1); 
+    }
+
+    const handleAddToCart = () => {
+      if(!selectedSize ||  !selectedColor) {
+        toast.error("Please select a size and color before adding to cart.", {
+          duration: 1000,
+        });
+        return;
+      }
+      setIsButtonDisabled(true);
+
+      setTimeout(() => {
+        toast.success("Product added to cart!", {
+          duration: 1000,
+        });
+        setIsButtonDisabled(false);
+      }, 500);
+    };
+
   return (
     <div className='p-6'>
       <div className='max-w-6xl mx-auto bg-white p-8 rounded-lg'>
@@ -86,8 +114,9 @@ const ProductDetails = () => {
               <div className='flex gap-2 mt-2'>
                 {selectedProduct.colors.map((color) => (
                   <button 
-                    key={color} 
-                    className='w-8 h-8 rounded-full border'
+                    key={color}
+                    onClick={() => setSelectedColor(color)} 
+                    className={`w-8 h-8 rounded-full border ${selectedColor === color ? "border-4 border-gray-100" : "border-gray-300"}`}
                     style={{backgroundColor: color.toLocaleLowerCase(),
                         filter: "brightness(0.5)",
                     }}
@@ -104,7 +133,8 @@ const ProductDetails = () => {
                 {selectedProduct.sizes.map((size) => (
                   <button 
                   key={size}
-                  className='px-4 py-2 rounded border'
+                  onClick={() => setSelectedSize(size)}
+                  className={`px-4 py-2 rounded border ${selectedSize === size ? " bg-black text-white" : ""}`}
                   >{size}</button>
                 ))}
               </div>
@@ -112,16 +142,25 @@ const ProductDetails = () => {
             <div className='mb-6'>
               <p className='text-gray-700'>Quantity</p>
               <div className='flex items-center space-x-4 mt-2'>
-                <button className='px-2 py-1 bg-gray-200 rounded text-lg'>
+                <button 
+                  onClick={() => handleQuantityChange("minus")}
+                  className='px-2 py-1 bg-gray-200 rounded text-lg'>
                   -
                 </button>
-                <span className='text-lg'>1</span>
-                <button className='px-2 py-1 bg-gray-200 rounded text-lg'>
+                <span className='text-lg'>{quantity}</span>
+                <button 
+                  onClick={() => handleQuantityChange("plus")}
+                  className='px-2 py-1 bg-gray-200 rounded text-lg'>
                   +
                 </button>
               </div>
             </div>
-            <button className='bg-black text-white py-2 px-6 rounded w-full mb-4'>Add To Cart</button>
+            <button 
+              onClick={handleAddToCart}
+              disabled={isButtonDisabled}
+              className={`bg-black text-white py-2 px-6 rounded w-full mb-4 ${isButtonDisabled ? "cursor-not-allowed apacity-50" : "hover: bg-gray-900"}`}>
+                {isButtonDisabled ? "Adding..." : "Add To Cart"}
+                </button>
             <div className='mt-10 text-gray-700'>
               <h3 className='text-xl font-bold mb-4'>Characteristics:</h3>
               <table className='w-full text-left text-sm text-gray-600'>
@@ -138,6 +177,9 @@ const ProductDetails = () => {
               </table>
             </div>
           </div>
+        </div>
+        <div className='mt-20'>
+          <h2 className='text-2xl text-center'></h2>
         </div>
       </div>
     </div>
